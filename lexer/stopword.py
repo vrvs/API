73a6569e189125939b034b36069741673e1e6455
Lucas Alves Rufino <lar@cin.ufpe.br>
2017-11-06 20:16:29 +0000

    Modulo de stopwords no pacote lexer implementado

--- lexer/stopword.py
+++ lexer/stopword.py
@@ -1,20 +1,45 @@
 # encoding: utf-8
 import json
 
-_sw = {}
+_sw = set()
 
-def load(path = "stopwords/stopwords.json"):
+def load(path = "lexer/stopwords/stopwords.json"):
+    global _sw
     with open(path, 'r') as fileIn:
-        _sw = json.load(fileIn)
+        words = json.load(fileIn)
+        _sw = set(words)
         fileIn.close()
-    return _sw
 
-def save(path = "stopwords/stopwords.json"):
+def save(path = "lexer/stopwords/stopwords.json"):
+    words = list(_sw)
     with open(path, 'w') as fileOut:
-        json.dump(_sw, fileOut)
+        json.dump(words, fileOut)
         fileOut.close()
 
+def transform(query):
+    newQuery = []
+    for word in query:
+        if word not in _sw:
+            newQuery.append(word)
+    return newQuery
+
 def contains(word):
+    flag = word in _sw
+    return flag
+
+def words():
+    ls = list(_sw)
+    return ls
+
+def size():
+    size = len(_sw)
+    return size
+
+def insert(word):
+    _sw.add(word)
+
+def remove(word):
+    _sw.discard(word)
     
-
-
+def clear():
+    _sw.clear()
